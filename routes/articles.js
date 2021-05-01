8d2b62a57a49c79ca239dbaf6686a34571df1298
orsaada <orsaa@post.bgu.ac.il>
2021-05-01 23:20:54 +0300

    fixed delete

--- routes/articles.js
+++ routes/articles.js
@@ -31,7 +31,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.delete(":id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
   await Article.findByIdAndDelete(req.params.id);
   res.redirect("/");
 });
